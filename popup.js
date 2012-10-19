ba645af33d626e67f95853e24d6c1ebe28c97ebd
Guitz <webguitou@gmail.com>
2012-10-19 19:25:05 +0200

    Ultimate Timer

--- popup.js
+++ popup.js
@@ -9,6 +9,38 @@ var socket = io.connect('http://protected-bastion-9703.herokuapp.com');
 chrome.extension.sendMessage({cmd : "refresh"});
 socket.emit("list_group");
 
+
+var date_updater;
+function start_date_updater() {
+	console.log("starting date-updater");
+	date_updater = setInterval(function() {
+		$("#liens ul li").each(function(index, element){
+			post_time = $(this).data("timestamp");
+			if( post_time != "undefined"){
+				mnt = new Date();
+				ms = (mnt.getTime()-Date.parse(post_time));
+				x = ms / 1000;
+				seconds = Math.floor(x % 60);
+				x /= 60;
+				minutes = Math.floor(x % 60);
+				x /= 60;
+				hours = Math.floor(x % 24);
+				x /= 24,
+				days = Math.floor(x);
+				var output;
+				if( days > 0) {	output = days + " jours"}else if( hours > 0) { output = hours + " heures"}else if( minutes > 0) { output = minutes + " minutes"}else if( seconds > 0) { output = seconds + " secondes"};
+				$("a .date",this).html(' il y a ' + output);
+			}
+		})
+	}, 1000);
+}
+
+function stop_date_updater(){
+	console.log('stop');
+	clearInterval(date_updater);
+}
+
+
 /*
  *	Login
  *
@@ -34,19 +66,22 @@ chrome.extension.onMessage.addListener(
 	if ( message.cmd == "links") {
 		data = message.links;
 		chrome.browserAction.setBadgeText({text: "<-"});
+		stop_date_updater();
 		$("#liens>ul").html("");
 		$(data).each(function(index, element){
 			var disabled = "'";
-			console.log(jQuery.inArray(localStorage.user, element.votes));
 			if( jQuery.inArray(localStorage.user, element.votes) != -1) { 
 				disabled = " liked' disabled='disabled'";
 			}
-			$("#liens>ul").append("<li data-id='" + element._id + "' ><div class='transition_all'><input type='image' id='like_button' src='images/empty.png' class='transition_opacity"+disabled+"' /><b class='transition_opacity'>" + element.votes.length + "</b></div><a href='" + element.url + "' target='_blank' title='" + element.title + "'><h2>" + element.title + "</h2><span>"+ element.comment +"</span><em>" + element.user + "</em></a></li><div class='clear'></div><img src='images/border_bottom.png'>");
-
+			$("#liens>ul").append("<li data-id='" + element._id + "' data-timestamp='"+ element.timestamp +"' ><div class='transition_all'><input type='image' id='like_button' src='images/empty.png' class='transition_opacity"+disabled+"' /><b class='transition_opacity'>" + element.votes.length + "</b></div><a href='" + element.url + "' target='_blank' title='" + element.title + "'><h2>" + element.title + "</h2><span>"+ element.comment +"</span><em>" + element.user + "</em><em class='date'></em></a></li><div class='clear'></div><img src='images/border_bottom.png'>");
+			mnt = new Date();
+			post = Date.parse(element.timestamp);
+		
 		})
 		localStorage.liens = $("#liens>ul").html();
 		localStorage.notif = 0;
 		chrome.browserAction.setBadgeText({text: ""});
+		start_date_updater();
 	}
 	if ( message.cmd == "likes") {
 		data = message.likes;
